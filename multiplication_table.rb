ccd7b7e0880b5eb77ba171a860d5b9f27521784a
David Gustafson <git@radicalskeptic.net>
2011-12-26 13:29:55 -0800

    not sure.

--- multiplication_table.rb
+++ multiplication_table.rb
@@ -13,7 +13,7 @@ def generate_row(row_number, number_of_cells)
   column = 2
   cell_size = (row_number * row_number).size
   (1..number_of_cells).each do |column|
-    row << generate_cell(column, row_number, cell_size)
+    row &lt;&lt; generate_cell(column, row_number, cell_size)
   end
   row + "\n"
 
@@ -22,31 +22,39 @@ end
 def generate_table(integer)
   table = ""
   (1...integer).each do |row_number|
-    table << generate_row(row_number, integer)
+    table &lt;&lt; generate_row(row_number, integer)
   end
   table
 end
 
-
+def add_decoration(decorate, decoration, table_string)
+  table_string &lt;&lt; decoration + "\n"
+end
 
 def multiplication_table(integer, heading = '', decorate = false)
+  unless (integer.is_a?(Fixnum) and heading.is_a?(String))
+    puts "first argument must be an integer"
+    exit
+  end
   table_string = ""
-  decoration = "="
   decoration_length = (get_cell_size(integer) * integer) + 2
+  decoration = ("=" * decoration_length)
+
 
   if heading
-    table_string << heading + "\n"
+    table_string &lt;&lt; heading + "\n"
   end
 
   if decorate
-    table_string << (decoration * decoration_length) + "\n"
+    add_decoration(decorate, decoration, table_string)
+
+    table_string &lt;&lt; generate_table(integer)
+
+    add_decoration(decorate, decoration, table_string)
+  else
+    table_string &lt;&lt; generate_table(integer)
   end
 
-  table_string << generate_table(integer)
-
-  if decorate
-    table_string << (decoration * decoration_length) + "\n"
-  end
 
   table_string
 end
