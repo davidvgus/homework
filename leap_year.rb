15a7797c703bf2892140577104be858b8eeaaa82
David Gustafson <git@radicalskeptic.net>
2011-12-30 21:14:52 -0800

    Created the method and first test.

---
+++ leap_year.rb
@@ -0,0 +1,22 @@
+=begin
+accept a year value from the user, check whether
+it's a leap year, and then return true or false.
+With the help of this leap_year?() method calculate
+and display the number of minutes in a leap year
+(2000 and 2004) and the number of minutes in a
+non-leap year (1900 and 2005). Note: a century
+year, like 1900 and 2000, is a leap year only if
+it is divisible by 400.
+=end
+
+=begin
+doctest: First test will return nil if no year was
+passed into the method
+>> leap_year?()
+=> nil
+REMEMBER TO MAKE NEW TESTS
+=end
+
+def leap_year?(year = nil)
+  year
+end
