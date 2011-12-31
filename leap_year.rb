8327cd90b42fdf97e4b3545550df7b90f14ad105
David Gustafson <git@radicalskeptic.net>
2011-12-31 01:07:11 -0800

    largely finished leap_year? function.

--- leap_year.rb
+++ leap_year.rb
@@ -1,3 +1,5 @@
+require 'win32console' if RUBY_PLATFORM =~ /mingw/
+
 =begin
 accept a year value from the user, check whether
 it's a leap year, and then return true or false.
@@ -10,13 +12,55 @@ it is divisible by 400.
 =end
 
 =begin
-doctest: First test will return nil if no year was
-passed into the method
+doctest: test divisible_by? with 2000 and 4
+>> divisible_by?(2000, 4)
+=> true
+doctest: test divisible_by?  with 2001 4
+>> divisible_by?(2001, 4)
+=> false
+=end
+
+def divisible_by?(num = nil, denominator)
+  if num % denominator == 0
+    true
+  else
+    false
+  end
+end
+
+
+=begin
+doctest: First test will return nil if no year was passed into the method
 >> leap_year?()
 => nil
+doctest: test on 2001
+>> leap_year?(2001)
+=> false
+doctest: test on 2004
+>> leap_year?(2004)
+=> true
+doctest: test on 2000
+>> leap_year?(2000)
+=> true
+doctest: test on 2400
+>> leap_year?(2400)
+=> true
 REMEMBER TO MAKE NEW TESTS
 =end
 
+
+
+
 def leap_year?(year = nil)
-  year
+  if year.nil?
+    nil
+  elsif year.is_a?(Fixnum) && year.to_s.length == 4
+    if divisible_by?(year, 400)
+      true
+    elsif !divisible_by?(year, 100) && divisible_by?(year, 4)
+      true
+    else
+      false
+    end
+  end
 end
