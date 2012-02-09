8ac7ee18b47096838844f396e8be3bfc05abb2ce
David Gustafson <git@radicalskeptic.net>
2012-02-08 18:43:02 -0800

    minor changes to convert.rb

--- convert.rb
+++ convert.rb
@@ -1,3 +1,5 @@
+#convert.rb
+
 #converts degrees Fahrenheit to degrees Celcius
 def convert(fdegrees)
   celcius = (fdegrees.to_f - 32.0) * (5.0/9.0)
@@ -11,5 +13,3 @@ input = gets.chomp()
 degrees_celcius = convert(input)
 
 printf ("#{input} degrees Fahrenheit is equal to %.2f degrees Celcius\n" % degrees_celcius)
-
-#http://flori.github.com/term-ansicolor/
