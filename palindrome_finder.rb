15f27adee40994f31ccbc23db59b07536a1ed78d
David Gustafson <git@radicalskeptic.net>
2012-05-10 00:46:16 -0700

    first version of palindrome finder done.

--- palindrome_finder.rb
+++ palindrome_finder.rb
@@ -1,11 +1,18 @@
 string = ARGV[0]
 
 x = 3 
+longest_match = ""
+
 string.length.times do
   string.length.times do |n2|
     test_string = string.slice(n2, x)
-    bleh = test_string.scan(/#{test_string.reverse}/)[0]
-    puts bleh if bleh.length > 2
+    result = test_string.scan(/#{test_string.reverse}/)[0]
+    if !result.nil? && result.length > 2
+      #puts result
+      longest_match = result if result.length > longest_match.length
+    end
   end
-  x += 1
+  x += 1 
 end
+
+puts longest_match
